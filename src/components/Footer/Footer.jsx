3a488b1198742a70ac075cf84c1620bffbe68309
Akash Bhosale <bhosaleakash3915@gmail.com>
2022-02-15 16:48:17 +0530

    gpt-3 complete

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,33 +8,37 @@ const Footer = () => {
         <h1 className="footer__gradient">
           Do you want to step in to the future before others
         </h1>
-        <button className="footer__request">Request Early Access</button>
+        <div className="req__btn">
+          <button className="footer__request">Request Early Access</button>
+        </div>
         <div className="footer__row">
           <div className="footer__column">
-            <h1>GPT-3</h1>
-            <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
+            <h1 className="footer__logo">GPT-3</h1>
+            <p className="footer__p address">
+              Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved
+            </p>
           </div>
           <div className="footer__column">
-            <h1>Links</h1>
-            <p>Overons</p>
-            <p>Social Media</p>
-            <p>Counters</p>
-            <p>Contact</p>
+            <h1 className="footer__h">Links</h1>
+            <p className="footer__p">Overons</p>
+            <p className="footer__p">Social Media</p>
+            <p className="footer__p">Counters</p>
+            <p className="footer__p">Contact</p>
           </div>
           <div className="footer__column">
-            <h1>Company</h1>
-            <p>Terms & Conditions</p>
-            <p>Privacy Policy</p>
-            <p>Contact</p>
+            <h1 className="footer__h">Company</h1>
+            <p className="footer__p">Terms & Conditions</p>
+            <p className="footer__p">Privacy Policy</p>
+            <p className="footer__p">Contact</p>
           </div>
           <div className="footer__column">
-            <h1>Get in touch</h1>
-            <p>Crechterwoord K12 182 DK Alknjkcb</p>
-            <p>085-132567</p>
-            <p>info@payme.net</p>
+            <h1 className="footer__h">Get in touch</h1>
+            <p className="footer__p">Crechterwoord K12 182 DK Alknjkcb</p>
+            <p className="footer__p">085-132567</p>
+            <p className="footer__p">info@payme.net</p>
           </div>
         </div>
-        <p>© 2021 GPT-3. All rights reserved.</p>
+        <p className="copyright">© 2021 GPT-3. All rights reserved.</p>
       </div>
     </React.Fragment>
   );
